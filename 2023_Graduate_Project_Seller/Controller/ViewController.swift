becf77866ec790253ae00fd52ce1386137454996
DongHyunW0n <68365742+DongHyunW0n@users.noreply.github.com>
2023-05-20 16:19:14 +0900

    Initial Commit

---
+++ 2023_Graduate_Project_Seller/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2023_Graduate_Project_Seller
+//
+//  Created by WonDongHyun on 2023/05/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
