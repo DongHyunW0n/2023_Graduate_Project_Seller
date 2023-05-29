cdcf4c3b98a0616348c1db19a44f6dcc2aa7c428
DongHyunW0n <68365742+DongHyunW0n@users.noreply.github.com>
2023-05-30 01:30:07 +0900

    내 리스트에 뜨게 수정

---
+++ 2023_Graduate_Project_Seller/Controller/MyBidListDetailViewController.swift
@@ -0,0 +1,62 @@
+//
+//  MyBidListDetailViewController.swift
+//  2023_Graduate_Project_Seller
+//
+//  Created by WonDongHyun on 2023/05/30.
+//
+
+import UIKit
+
+class MyBidListDetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var placeLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var detailLabel: UITextView!
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
+    var bidPostList: BidPostEntity?
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let imageURL = bidPostList?.imageURL {
+            imageView.loadImageFromURL(imageURL)
+        }
+        updateUI()
+
+    }
+    
+
+    func updateUI() {
+            if let bidPost = bidPostList {
+                placeLabel.text = bidPost.address // 주소 설정
+                dateLabel.text = bidPost.date // 날짜 설정
+                detailLabel.text = bidPost.title // 상세 설명 설정
+                numberLabel.text = bidPost.number // 연락처 설정
+            }
+        }
+
+}
+
+
+extension UIImageView {
+    func loadImageFromURL1(_ urlString: String) {
+        guard let url = URL(string: urlString) else {
+            // 유효하지 않은 URL 처리
+            return
+        }
+        
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
--- 2023_Graduate_Project_Seller/Controller/MyBidListViewController.swift
+++ 2023_Graduate_Project_Seller/Controller/MyBidListViewController.swift
@@ -10,161 +10,140 @@ import FirebaseDatabase
 import FirebaseAuth
 
 struct MyBidListEntity {
-    
-    var title : String
-    var address : String
-    var imageURL : String
-    var date : String
-    var number : String
-    
-    
+    var title: String
+    var address: String
+    var imageURL: String
+    var date: String
+    var number: String
 }
 
 struct FinishedEntity {
-    
-    var bidId : String
-    var postID : String
-    var detail : String
-    
+    var bidId: String
+    var postID: String
+    var detail: String
 }
 
-
+struct BidPostEntity {
+    var title: String
+    var address: String
+    var imageURL: String
+    var date: String
+    var number: String
+}
 
 class MyBidListViewController: UIViewController {
-    
-    var myBidList : [MyBidListEntity] = []
-    var myFinishedListEntity : [FinishedEntity] = []
-    
+    var myBidList: [FinishedEntity] = []
+    var bidPostList: [BidPostEntity] = []
+
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var titleLabel: UILabel!
-    
-    
-    
+
     let ref = Database.database().reference()
     let uid = Auth.auth().currentUser?.uid
-    
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         tableView.dataSource = self
         tableView.delegate = self
-        
-        
+
         titleLabel.layer.cornerRadius = 20
-        
+
         var companyName = "동서철물"
-        ref.child("Users").child(self.uid ?? "UID ERROR").child("name").observeSingleEvent(of: .value) { (snapshot) in //옵셔빙 싱글 이벤트는 한번만 받아옴.
+        ref.child("Users").child(self.uid ?? "UID ERROR").child("name").observeSingleEvent(of: .value) { (snapshot) in
             if let name = snapshot.value as? String {
-                print("name: \(name)") //회사이름
-                
+                print("name: \(name)") // 회사이름
+
                 companyName = name
                 self.titleLabel.text = "\(name) 사장님께서 낙찰받으신 내역은 다음과 같습니다."
-                
-                
+
+                self.fetchFinishedBids(for: companyName)
             }
-            
         }
-        
-        print("회사의 이름은 \(companyName)")
-        
-        
-        
-        
-        
-        
-        
-        
-        ref.child("FinishedBid").child("\(companyName)").observe(.value) {snapshot in
-            
-            
-            
-            
-            self.myFinishedListEntity = []
-            
+    }
+
+    func fetchFinishedBids(for companyName: String) {
+        ref.child("FinishedBid").child("\(companyName)").observe(.value) { snapshot in
+            var finishedBids: [FinishedEntity] = []
+            var bidPosts: [BidPostEntity] = []
+
             for child in snapshot.children {
-                
-                guard let childSnapshot = child as? DataSnapshot else{return}
+                guard let childSnapshot = child as? DataSnapshot else { return }
                 let value = childSnapshot.value as? NSDictionary
+                let title = value?["상세 설명"] as? String ?? ""
                 let bidID = value?["bidID"] as? String ?? ""
-                let postID = value?["postID"]as? String ?? ""
-                let detail = value?["상세 설명"]as? String ?? ""
-                
-                let fetchedBidList = FinishedEntity(bidId: bidID, postID: postID, detail: detail)
-                self.myFinishedListEntity.append(fetchedBidList)
-            
-                print("내가 확정받은 견적의 개수 : \(self.myFinishedListEntity.count)")
-                
-                print("확정된 견적들에 대한 딕셔너리 값들 :  \(self.myFinishedListEntity)")
-                
-                
+                let postID = value?["postID"] as? String ?? ""
 
+                let fetchedBid = FinishedEntity(bidId: bidID, postID: postID, detail: title)
+                finishedBids.append(fetchedBid)
+                print(fetchedBid)
 
-                
-//                self.ref.child("ServiceRequest").child("\(mypostID)").observe(.value) {snapshot in
-//
-//
-//                    self.myBidList = []
-//
-//                    for child in snapshot.children {
-//
-//                        guard let childSnapshot = child as? DataSnapshot else{return}
-//                        let value = childSnapshot.value as? NSDictionary
-//                        let title = value?["상세 설명"] as? String ?? ""
-//                        let address = value?["요청 위치"]as? String ?? ""
-//                        let imageURL = value?["사진 URL"]as? String ?? ""
-//                        let date = value?["요청 일시"]as? String ?? ""
-//                        let number = value?["연락처"]as? String ?? ""
-//
-//                        let fetchedMyBidList = MyBidListEntity(title: title, address: address, imageURL: imageURL, date: date, number: number)
-//
-//                        self.myBidList.append(fetchedMyBidList)
-//
-//                        print("확정된 견적에 대한 데이터는 \(fetchedMyBidList)")
-//        //                print(fetchedBidList)
-//                    }
-//
-////                    self.tableView.reloadData()
-//
-//                }
-                
-                
-                
-                
-                
-                
+                self.ref.child("ServiceRequest").child(postID).observeSingleEvent(of: .value) { postSnapshot in
+                    if let postValue = postSnapshot.value as? NSDictionary {
+                        let postTitle = postValue["상세 설명"] as? String ?? ""
+                        let postAddress = postValue["요청 위치"] as? String ?? ""
+                        let postImageURL = postValue["사진 URL"] as? String ?? ""
+                        let postDate = postValue["요청 일시"] as? String ?? ""
+                        let postNumber = postValue["연락처"] as? String ?? ""
+
+                        let bidPost = BidPostEntity(title: postTitle, address: postAddress, imageURL: postImageURL, date: postDate, number: postNumber)
+                        bidPosts.append(bidPost)
+                        print(bidPosts)
+
+                        if bidPosts.count == finishedBids.count {
+                            self.bidPostList = bidPosts
+                            self.tableView.reloadData()
+                        }
+                    }
+                }
             }
+
+            self.myBidList = finishedBids
+        }
+    }
+}
+
+extension MyBidListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myBidList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! MyBidListCell
+
+        let bid = myBidList[indexPath.row]
+        let bidPost = bidPostList[indexPath.row]
+
+        cell.titleLabel.text = "견적 : \(bid.detail)"
+        cell.addressLabel.text = bidPost.address
+        cell.timeLabel.text = "방문요청 :\(bidPost.date)"
+        cell.numberLabel.text = "고객 연락처 :\(bidPost.number)"
+
+        cell.selectionStyle = .none
+
+        // 다른 셀 속성 설정
+
+        return cell
+    }
+}
+
+extension MyBidListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        
+
+        
+        let selectedBid = bidPostList[indexPath.row]
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let bidDetailVC = storyboard.instantiateViewController(withIdentifier: "MyBidListDetailViewController") as? MyBidListDetailViewController {
+            bidDetailVC.bidPostList = selectedBid
+           
+            navigationController?.pushViewController(bidDetailVC, animated: true)
+            
             
             
         }
-        
-        
-        
-        
-    }
-}
-
-
-extension MyBidListViewController : UITableViewDataSource {
-    
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return
-    }
-    
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return
-    }
-    
-    
-}
-
-extension MyBidListViewController : UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
     }
 }
